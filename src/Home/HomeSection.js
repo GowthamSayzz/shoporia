171b27fbbe1a44cfef53459d042e28d06d40e7e8
Gowtham Kunjeti <gkunjeti@gmail.com>
2025-09-28 12:05:35 +0530

    homepage responsiveness modifications

--- src/Home/HomeSection.js
+++ src/Home/HomeSection.js
@@ -41,22 +41,22 @@ function HomeSection() {
             <div id="autoCarousel" className="carousel slide" data-bs-ride="carousel" data-bs-interval="3000" data-bs-pause="false" >
                 <div className="carousel-inner" style={{ aspectRatio: "211 / 35" }}>
                     <div className="carousel-item active">
-                        <img src={banner1} className="d-block w-100" alt="Slide 1" />
+                        <img src={banner1} className="d-block w-100 img-fluid" alt="Slide 1" />
                     </div>
                     <div className="carousel-item">
-                        <img src={banner2} className="d-block w-100" alt="Slide 2" />
+                        <img src={banner2} className="d-block w-100 img-fluid" alt="Slide 2" />
                     </div>
                     <div className="carousel-item">
-                        <img src={banner3} className="d-block w-100" alt="Slide 3" />
+                        <img src={banner3} className="d-block w-100 img-fluid" alt="Slide 3" />
                     </div>
                     <div className="carousel-item">
-                        <img src={banner4} className="d-block w-100" alt="Slide 4" />
+                        <img src={banner4} className="d-block w-100 img-fluid" alt="Slide 4" />
                     </div>
                     <div className="carousel-item">
-                        <img src={banner5} className="d-block w-100" alt="Slide 5" />
+                        <img src={banner5} className="d-block w-100 img-fluid" alt="Slide 5" />
                     </div>
                     <div className="carousel-item">
-                        <img src={banner6} className="d-block w-100" alt="Slide 6" />
+                        <img src={banner6} className="d-block w-100 img-fluid" alt="Slide 6" />
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#autoCarousel" data-bs-slide="prev">
@@ -69,8 +69,8 @@ function HomeSection() {
                 </button>
             </div>
             <div className='container mt-3'>
-                <div className='row'>
-                    <div className='col-10 shadow'>
+                <div className='row g-3'>
+                    <div className='col-12'>
                         <div id="phonesCarousel" className="carousel slide" data-bs-interval="3000" data-bs-ride="carousel" data-bs-pause="false">
                             <div className="carousel-inner">
                                 {getPhones.reduce((rows, phone, index) => {
@@ -80,12 +80,12 @@ function HomeSection() {
                                     return rows;
                                 }, []).map((group, i) => (
                                     <div key={i} className={`carousel-item ${i === 0 ? 'active' : ''}`}>
-                                        <div className="d-flex justify-content-center mt-4">
+                                        <div className="d-flex flex-wrap justify-content-center mt-4">
                                             {group.map((phone, j) => (
-                                                <div key={j} className="card me-3" style={{ width: '200px' }}>
+                                                <div key={j} className="card me-3 mb-3" style={{ width: '200px' }}>
                                                     <img
                                                         src={phone.thumbnail}
-                                                        className="card-img-top pt-3 product-card"
+                                                        className="card-img-top pt-3 product-card img-fluid"
                                                         alt={phone.title}
                                                         style={{ height: '200px', objectFit: 'contain' }}
                                                     />
@@ -111,45 +111,39 @@ function HomeSection() {
                             </button>
                         </div>
                     </div>
-                    <div className='col-2'>
-                        <div className='card-body shadow'>
-                            <img src={banner8} alt='image-poster' style={{ height: '335px', width: '205px' }} />
+                    {/* <div className='col-12 col-lg-2'>
+                        <div className='card-body shadow h-100 d-flex justify-content-center align-items-center'>
+                            <img src={banner8} alt='image-poster' className='img-fluid' style={{ maxheight: '335px', objectFit:'cover' }} />
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
 
             <div className='container mt-3 shadow border'>
                 <div className='row'>
                     <h6 className='fs-4 mt-3 mb-3'>Featured Products</h6>
-                    <div id="brandsCarousel" className="carousel slide" data-bs-interval="3000" data-bs-ride="carousel" data-bs-pause="false">
+                    <div id="brandsCarousel" className="carousel slide mb-3" data-bs-interval="3000" data-bs-ride="carousel" data-bs-pause="false">
                         <div className="carousel-inner">
-                            <div className="carousel-item active mb-3">
-                                <div className="d-flex justify-content-start">
-                                    <img src={banner9} className="img-fluid me-2 product-card" style={{ width: '312px' }} alt="brand1" />
-                                    <img src={banner10} className="img-fluid me-2 product-card" style={{ width: '312px' }} alt="brand2" />
-                                    <img src={banner11} className="img-fluid me-2 product-card" style={{ width: '312px' }} alt="brand3" />
-                                    <img src={banner12} className="img-fluid me-2 product-card" style={{ width: '312px' }} alt="brand4" />
+                            {[
+                                [banner9, banner10, banner11, banner12],
+                                [banner13, banner14, banner15, banner16],
+                                [banner17, banner18, banner19, banner20]
+                            ].map((group, i) => (
+                                <div key={i} className={`carousel-item ${i === 0 ? 'active' : ''}`}>
+                                    <div className="row g-1">
+                                        {group.map((img, j) => (
+                                            <div key={j} className="col-6 col-md-3">
+                                                <img
+                                                    src={img}
+                                                    className="img-fluid w-100 d-block"
+                                                    alt={`brand-${i}-${j}`}
+                                                    style={{ height: '200px', objectFit: 'cover' }}
+                                                />
+                                            </div>
+                                        ))}
+                                    </div>
                                 </div>
-                            </div>
-
-                            <div className="carousel-item mb-3">
-                                <div className="d-flex justify-content-start">
-                                    <img src={banner13} className="img-fluid me-2 product-card" style={{ width: '312px' }} alt="brand1" />
-                                    <img src={banner14} className="img-fluid me-2 product-card" style={{ width: '312px' }} alt="brand2" />
-                                    <img src={banner15} className="img-fluid me-2 product-card" style={{ width: '312px' }} alt="brand3" />
-                                    <img src={banner16} className="img-fluid me-2 product-card" style={{ width: '312px' }} alt="brand4" />
-                                </div>
-                            </div>
-
-                            <div className="carousel-item mb-3">
-                                <div className="d-flex justify-content-start">
-                                    <img src={banner17} className="img-fluid me-2 product-card" style={{ width: '312px' }} alt="brand1" />
-                                    <img src={banner18} className="img-fluid me-2 product-card" style={{ width: '312px' }} alt="brand1" />
-                                    <img src={banner19} className="img-fluid me-2 product-card" style={{ width: '312px' }} alt="brand1" />
-                                    <img src={banner20} className="img-fluid me-2 product-card" style={{ width: '312px' }} alt="brand1" />
-                                </div>
-                            </div>
+                            ))}
                         </div>
 
                         <button className="carousel-control-prev" type="button" data-bs-target="#brandsCarousel" data-bs-slide="prev">
@@ -165,12 +159,12 @@ function HomeSection() {
                 </div>
             </div>
             <div className='container mt-3'>
-                <div className='row'>
-                    <div className='col-6'>
-                        <img src={banner21} alt='images' style={{height:'100%', width:'100%'}}/>
+                <div className='row g-3'>
+                    <div className='col-12 col-md-6'>
+                        <img src={banner21} alt='images' className='img-fluid w-100 h-100' />
                     </div>
-                    <div className='col-6'>
-                        <img src={banner22} alt='images' style={{height:'100%', width:'100%'}} />
+                    <div className='col-12 col-md-6'>
+                        <img src={banner22} alt='images' className='img-fluid w-100 h-100' />
                     </div>
                 </div>
             </div>
